9b77be1f9629ea7f4ebb56954ea5d6c7b0f14bbd
sanojan <shanojan.ar@gmail.com>
2020-05-10 21:35:46 +0530

    Added seeder file for designation & services

--- database/seeds/DesignationSeeder.php
+++ database/seeds/DesignationSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Designation;
 
 class DesignationSeeder extends Seeder
 {
@@ -11,8 +12,57 @@ class DesignationSeeder extends Seeder
      */
     public function run()
     {
-        $designations = [['id' => '1', 'name' => 'Divisional Secretary'], ['id' => '2', 'name' => 'Chief Management Assistant'], ['id' => '3', 'name' => 'Accountant'], ['id' => '4', 'name' => 'ICT Assistant'], ['id' => '5', 'name' => 'Public Management Officer']];
+        $designations = 
+        [
+            ['id' => '1', 
+             'name' => 'Divisional Secretary'
+            ],
+
+            ['id' => '2', 
+             'name' => 'Chief Management Assistant'
+            ], 
+
+            ['id' => '3', 
+             'name' => 'Accountant'
+            ], 
+            
+            ['id' => '4', 
+             'name' => 'ICT Assistant'
+            ], 
+            
+            ['id' => '5', 
+             'name' => 'Management Service Officer'
+            ],
+
+            ['id' => '6', 
+            'name' => 'Development Officer'
+            ],
+
+            ['id' => '7', 
+            'name' => 'Finance Assistant'
+            ],
+
+            ['id' => '8', 
+            'name' => 'Administrative Officer'
+            ],
+
+            ['id' => '9', 
+            'name' => 'Deputy Director of Planning'
+            ],
+
+           ['id' => '10', 
+           'name' => 'Technical Officer'
+           ],
+
+           ['id' => '11', 
+           'name' => 'Cultural Officer'
+           ],
+        ];
         
-        \App\Designation::create($designations);
+        //\App\
+
+        foreach($designations as $designation){
+            Designation::create($designation);
+        }
     }
 }
---
+++ database/seeds/ServiceSeeder.php
@@ -0,0 +1,79 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Service;
+
+class ServiceSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $services = 
+        [
+            ['id' => '1', 
+             'name' => 'SRI LANKA ADMINISTRATIVE SERVICE'
+            ],
+
+            ['id' => '2', 
+             'name' => 'SRI LANKA ENGINEERING SERVICE'
+            ], 
+
+            ['id' => '3', 
+             'name' => 'SRI LANKA ACCOUNTANTS SERVICE'
+            ], 
+            
+            ['id' => '4', 
+             'name' => 'SRI LANKA PLANNING SERVICE'
+            ], 
+            
+            ['id' => '5', 
+             'name' => 'SRI LANKA SCIENTIFIC SERVICE'
+            ],
+
+            ['id' => '6', 
+            'name' => 'SRI LANKA ARCHITECTURAL SERVICE'
+            ],
+
+            ['id' => '7', 
+            'name' => 'SRI LANKA INFORMATION & COMMUNICATION TECHNOLOGY SERVICE'
+            ],
+
+            ['id' => '8', 
+            'name' => 'TRANSLATORS SERVICE'
+            ],
+
+            ['id' => '9', 
+            'name' => 'LIBRARIANS SERVICE'
+            ],
+
+           ['id' => '10', 
+           'name' => 'DEVELOPMENT OFFICERS SERVICE'
+           ],
+
+           ['id' => '11', 
+           'name' => 'MANAGEMENT SERVICE OFFICER SERVICE'
+           ],
+
+           ['id' => '12', 
+           'name' => 'DRIVERS SERVICE'
+           ],
+
+           ['id' => '13', 
+           'name' => 'OFFICE EMPLOYEE SERVICE'
+           ],
+
+           ['id' => '14', 
+           'name' => 'SRI LANKA TECHNOLOGICAL SERVICE'
+           ],
+
+        ];
+
+        foreach($services as $service){
+            Service::create($service);
+        }
+    }
+}
